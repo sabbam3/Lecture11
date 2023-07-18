9c6045fe78830c55ae6655bfa0e5ee05bb197589
makha <makha@DESKTOP-TBGBRFD>
2023-07-18 13:53:21 +0400

    Add project files.

---
+++ Person.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lecture11
+{
+    public class Person
+    {
+        public string Name { get; set; }
+        public static int Count = 0;
+        public Person()
+        {
+            Count++;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Lecture11;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.Write("Enter 1-7: ");
+        var myDay = (DayOfWeek)int.Parse(Console.ReadLine());
+        Console.WriteLine(myDay.ToString());
+        var myStatus = Status.Delivered;
+        switch(myStatus)
+        {
+            case Status.Sent: Console.WriteLine(Status.Sent); break;    
+            case Status.Deleted: Console.WriteLine(Status.Deleted); break;
+            case Status.Delivered: Console.WriteLine(Status.Delivered); break;
+        }
+        Person person0 = new Person();
+        Person person1 = new Person();
+        Person person2 = new Person();
+        Console.WriteLine($"Person count: {Person.Count}");
+    }
+}
